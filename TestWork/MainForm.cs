2f7ddb77fe469bd9bf9ff583acf7848fa4434b54
IvanovaAM <okshurochka@yandex.ru>
2023-11-19 12:37:51 +0300

    The visual display of graphs has been corrected. The filling and the cleaning of series has been carried out to the separate methods.

--- TestWork/MainForm.cs
+++ TestWork/MainForm.cs
@@ -28,27 +28,32 @@ namespace TestWork
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            chart1.Series[0].Points.DataBindY(data.DataForSeria1);
-            chart1.Series[1].Points.DataBindY(data.DataForSeria2);
-            chart1.Series[2].Points.DataBindY(data.DataForSeria3);
-
-            //делает недействительной всю поверхность элемента управления Chart и вызывает перерисовку элемента управления Chart
-            chart1.Invalidate();
+            FillSeriesData();
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            //очистка серий от данных
-            foreach (var series in chart1.Series)            
-                series.Points.Clear();
-
+            CleanSeriasData();
             data.GenerateNewData();//генерация новых данных для отрисовки: серии 1 и 2 генерятся полностью с новыми зниачениями,
                                    //серия 3 удаляет первую точку и добавляет в конец новое значение
+            FillSeriesData();
+        }
+
+        void FillSeriesData()
+        {
+            //заполнение серий данными
             chart1.Series[0].Points.DataBindY(data.DataForSeria1);
             chart1.Series[1].Points.DataBindY(data.DataForSeria2);
             chart1.Series[2].Points.DataBindY(data.DataForSeria3);
-            //Перерисовка только серий
+            //перерисовка только серий
             chart1.Series.Invalidate();
+        }
+
+        void CleanSeriasData()
+        {
+            //очистка серий от данных
+            foreach (var series in chart1.Series)
+                series.Points.Clear();
         }
     }
 }
