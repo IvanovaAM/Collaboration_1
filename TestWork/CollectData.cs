ce2f7e5926b60d6b26f24a81eb629bd77e11a987
Alexandra <71124156+IvanovaAM@users.noreply.github.com>
2023-11-20 13:54:09 +0300

    ixed variable names

--- TestWork/CollectData.cs
+++ TestWork/CollectData.cs
@@ -17,12 +17,12 @@ namespace TestWork
         //генерация новых данных для отображения
         public void GenerateNewData(int first = 1)
         {
-            var rnd = new Random();
+            var random = new Random();
 
-            var x1 = rnd.Next(-5, 5);
-            var x2 = rnd.Next(11, 21);
-            var y = rnd.Next(0, 5);
-            var seria = rnd.Next(2);
+            var x1 = random.Next(-5, 5);
+            var x2 = random.Next(11, 21);
+            var y = random.Next(0, 5);
+            var seria = random.Next(2);
 
             //для рандомного отображения серий 1 и 2 
             DataForSeria1 = (seria == 0) ? AddDataToList(x1, x2) : AddDataToList(x1, x2, y);
@@ -35,7 +35,7 @@ namespace TestWork
             }
             else //генерация данных для серии3 при инициализации класса
             {
-                for (int i = 0; i < rnd.Next(5, 10); i++)
+                for (int i = 0; i < random.Next(5, 10); i++)
                     DataForSeria3.Add(StorageFunctions.GetData() + ((i + 1) * (i + 2))); //генерация первоначального количества данных для серии3
             }
         }
@@ -43,21 +43,21 @@ namespace TestWork
         //заполнение листа данными с использованием функции GetData(int i)
         List<double> AddDataToList(int x1, int x2)
         {
-            var tmpList = new List<double>();
+            var temproryDataList = new List<double>();
 
             for (int i = x1; i < x2; i++)
-                tmpList.Add(StorageFunctions.GetData(i));
-            return tmpList;
+                temproryDataList.Add(StorageFunctions.GetData(i));
+            return temproryDataList;
         }
 
         //заполнение листа данными с использованием функции GetData(int i, int j)
         List<double> AddDataToList(int x1, int x2, int y)
         {
-            var tmpList = new List<double>();
+            var temproryDataList = new List<double>();
 
             for (int i = x1, j = y; i < x2; i++, j--)
-                tmpList.Add(StorageFunctions.GetData(i, j));
-            return tmpList;
+                temproryDataList.Add(StorageFunctions.GetData(i, j));
+            return temproryDataList;
         }
     }
 }
